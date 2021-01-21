100ce80e56e01612c270ef287c21f0b1d5339031
SizeR <vip.sanki007@bk.ru>
2021-01-21 21:42:23 +0300

    Добавьте файлы проекта.

---
+++ Kpo4310_kim.Lib/sourse/Accessory/IAccessoryListLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Kpo4310.kim.Lib
+{
+    public interface IAccessoryListLoader
+    {
+        List<Accessory> accessoryList { get; }
+        AccessoryListSplitFileLoader.LoadStatus status { get; }
+
+        void Execute();
+    }
+}
